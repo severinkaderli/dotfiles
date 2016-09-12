7dc8aee3855342d3712cd6c9feefb8346d153faf
Severin Kaderli <severin.kaderli@gmail.com>
2016-09-12 21:22:37 +0200

    Add .dircolors
    Signed-off-by: Severin Kaderli <severin.kaderli@gmail.com>

--- install.sh
+++ install.sh
@@ -8,16 +8,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Setting up git configuration
 ln -s "$DIR/.gitconfig" "$HOME/.gitconfig"
 
-
-
-
-# We link all important files
+# Setting up various configuration files
 ln -s "$DIR/.bash_profile" "$HOME/.bash_profile"
 ln -s "$DIR/.bashrc" "$HOME/.bashrc"
 ln -s "$DIR/.profile" "$HOME/.bashrc"
 ln -s "$DIR/.xinitrc" "$HOME/.xinitrc"
 ln -s "$DIR/.Xresources" "$HOME/.Xresources"
 ln -s "$DIR/.config/i3" "$HOME/.config/i3"
+ln -s "$DIR/.dircolors" "$HOME/.dircolors"
 
 # Symlinking scripts directory and giving execute permissions
 ln -s "$DIR/bin" "$HOME/bin"
