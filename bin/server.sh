c1edaffcbf2280747d1733a818b1ad48ae526dd5
Severin Kaderli <severin.kaderli@gmail.com>
2017-01-30 21:00:08 +0100

    Update server.sh
    Signed-off-by: Severin Kaderli <severin.kaderli@gmail.com>

--- bin/server.sh
+++ bin/server.sh
@@ -5,12 +5,12 @@
 # Usage: ./server.sh [start|stop]
 
 if [ "$1" == "start" ]; then
-	sudo systemctl start apache2.service
-	sudo systemctl start mysql.service
+	sudo systemctl start httpd.service
+	sudo systemctl start mariadb.service
 	sudo systemctl start php7.0-fpm.service
 elif [ "$1" == "stop" ]; then
-	sudo systemctl stop apache2.service
-	sudo systemctl stop mysql.service
+	sudo systemctl stop httpd.service
+	sudo systemctl stop mariadb.service
 	sudo systemctl stop php7.0-fpm.service
 else
 	echo "'$1' is an unknown option"
