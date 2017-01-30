11315e481a7ff794b87ba12b8506daa2fc16a63a
Severin Kaderli <severin.kaderli@gmail.com>
2017-01-30 21:20:32 +0100

    Remove php from server.sh
    Signed-off-by: Severin Kaderli <severin.kaderli@gmail.com>

--- bin/server.sh
+++ bin/server.sh
@@ -7,11 +7,9 @@
 if [ "$1" == "start" ]; then
 	sudo systemctl start httpd.service
 	sudo systemctl start mariadb.service
-	sudo systemctl start php7.0-fpm.service
 elif [ "$1" == "stop" ]; then
 	sudo systemctl stop httpd.service
 	sudo systemctl stop mariadb.service
-	sudo systemctl stop php7.0-fpm.service
 else
 	echo "'$1' is an unknown option"
 	exit 0
